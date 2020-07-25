318b311d72e2c699a09ac2d0a85ef5577d47ecc8
Jgom <jgom@correo.com>
2020-07-25 02:07:27 +0200

    Added docs

---
+++ docs/precache-manifest.8ac53501a0327069f16f749c1b8fa4c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f18c9230e37f84d2ed2823eb70e5c7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f7d348f1f831148be5a",
+    "url": "/static/css/main.b3989795.chunk.css"
+  },
+  {
+    "revision": "b617679593a3f0bd5eaa",
+    "url": "/static/js/2.eea0565d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eea0565d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7d348f1f831148be5a",
+    "url": "/static/js/main.ff211ca4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
